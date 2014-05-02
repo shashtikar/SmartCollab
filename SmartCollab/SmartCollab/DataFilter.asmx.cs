30f4d9f3f2f109ac8d599b46a16554855a74c88f
unknown <coolshashank1@gmail.com>
2014-05-02 01:37:09 -0700

    New methods added

--- SmartCollab/SmartCollab/DataFilter.asmx.cs
+++ SmartCollab/SmartCollab/DataFilter.asmx.cs
@@ -11,11 +11,15 @@ using Lucene.Net.Util;
 using Lucene.Net.Store;
 using Lucene.Net.Documents;
 using System.IO;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Collections;
 
 namespace SmartCollab
 {
     /// <summary>
-    /// Summary description for DataFilter
+    /// This is the service interface of the web service that would facilitate 
+    /// collaboration amongst tenants.
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
@@ -24,52 +28,233 @@ namespace SmartCollab
     // [System.Web.Script.Services.ScriptService]
     public class DataFilter : System.Web.Services.WebService
     {
+        Lucene.Net.Store.Directory luceneWorkingSet;
 
         [WebMethod]
-        public string HelloWorld()
+        public String searchService(String keyword)
         {
-            return "Hello World";
-        }
-        
-        [WebMethod]
-        public string filterString(String input)
-        {
-            return "Hello";
-        }
 
-        [WebMethod]
-        public void searchKeyword(String keyString)
-        {
-            StandardAnalyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
-            System.IO.Directory.CreateDirectory("IndexFS");
-            System.IO.FileStream index = System.IO.File.Create(@"IndexFS\index.sha");
-            Lucene.Net.Store.Directory index1 = new RAMDirectory();
-            IndexWriter iw = new IndexWriter(index1, analyzer, true, IndexWriter.MaxFieldLength.LIMITED);
-              
+            dataConn con = new dataConn();
+            string indexPath = @"C:\Users\Shashank\Documents\SD\Project\working\inputFileFS"; ;
+            IndexSearcher searcher = new IndexSearcher(FSDirectory.Open(indexPath));
+            QueryParser parser = new QueryParser(Lucene.Net.Util.Version.LUCENE_30, "description", new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30));
 
-            String fileName = "index";
-            String pathString = System.IO.Path.Combine("indexFS",fileName);
+            Query query = parser.Parse(keyword);
+            try
+            {
+                TopDocs topResults = searcher.Search(query, 10);
 
-            Document doc = new Document();
-            StreamReader reader = new StreamReader(@"IndexFS\index.sha");
+                ScoreDoc[] topHits = topResults.ScoreDocs;
+                String[] docList = new String[10];
 
+                for (int i = 0; i < topHits.Length; i++)
+                {
+                    int docsId = topHits[i].Doc;
+                    Document d = searcher.Doc(docsId);
+                    docList[i] = d.Get("serviceURL");
+                }
+
+                if (topHits.Length > 0 && (con.isPublished(docList[0])))
+                {
+                    return (docList[0]);
+                }
+                else
+                {
+                    return "noData";  
+                }
+            }
+            catch
+            {
+                return "noData";
+            }
+            finally
+            {
+                searcher.Dispose();
+            }
             
-            doc.Add(new Field("description", reader));
-            doc.Add(new Field("serviceID", "12345", Field.Store.YES, Field.Index.ANALYZED));
-
-            iw.AddDocument(doc);
-            iw.Optimize();
-            iw.Dispose();
         }
 
         [WebMethod]
-        public int searchResult(String keyword)
+        public String searchRequirement(String keyword)
         {
 
-            searcherModule sm = new searcherModule();
+            dataConn con = new dataConn();
+            string indexPath = @"C:\Users\Shashank\Documents\SD\Project\working\inputFileFS"; ;
+            IndexSearcher searcher = new IndexSearcher(FSDirectory.Open(indexPath));
+            QueryParser parser = new QueryParser(Lucene.Net.Util.Version.LUCENE_30, "req_description", new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30));
 
-            return sm.hitCounter(keyword);
+            Query query = parser.Parse(keyword);
+            try
+            {
+                TopDocs topResults = searcher.Search(query, 10);
+
+                ScoreDoc[] topHits = topResults.ScoreDocs;
+                String[] docList = new String[10];
+
+                for (int i = 0; i < topHits.Length; i++)
+                {
+                    int docsId = topHits[i].Doc;
+                    Document d = searcher.Doc(docsId);
+                    docList[i] = d.Get("tenant_id");
+                }
+
+                if (topHits.Length > 0)
+                {
+                    return (docList[0]);
+                }
+                else
+                {
+                    return "noData";
+                }
+            }
+            catch
+            {
+                return "noData";
+            }
+            finally
+            {
+                searcher.Dispose();
+            }
+
+        }
+
+
+        [WebMethod]
+        public int unpublish_service(int service_id)
+        {
+            dataConn con = new dataConn();
+            return con.unpublishService(service_id);
+        }
         
+        [WebMethod]
+        public int unpublishReq(int tenant_id, int req_id)
+        {
+            dataConn con = new dataConn();
+            if (con.getReqOwnerId(req_id) == tenant_id)
+            {
+                return con.unpublish_req(req_id);
+
+            }
+            else
+            {
+                return 400;
+            }
+        }
+
+        [WebMethod]
+        public int publishMyService(int tenant_id, String serviceURL, String description)
+        {
+            dataConn con = new dataConn();
+
+            if (con.publishService(serviceURL, description, tenant_id) == 200)
+            {
+                String indexPath = @"C:\Users\Shashank\Documents\SD\Project\working\inputFileFS";
+                
+                if (!System.IO.Directory.Exists(indexPath))
+                {
+                    System.IO.Directory.CreateDirectory(indexPath);
+                }
+
+                luceneWorkingSet = FSDirectory.Open(indexPath);
+
+                StandardAnalyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+
+                IndexWriter writer = new IndexWriter(luceneWorkingSet, analyzer, true, IndexWriter.MaxFieldLength.LIMITED);
+                Document doc = new Document();
+
+                doc.Add(new Field("description", description, Field.Store.YES, Field.Index.ANALYZED));
+                doc.Add(new Field("serviceURL", serviceURL, Field.Store.YES, Field.Index.NOT_ANALYZED));
+
+                writer.AddDocument(doc);
+                writer.Optimize();
+
+                writer.Dispose();
+                luceneWorkingSet.Dispose();
+                return 200;
+            }
+
+            else
+            {
+                return 400;
+            }
+
+        }
+
+        [WebMethod]
+        public ArrayList getOwnedServiceList(int tenant_id)
+        {
+            dataConn con = new dataConn();
+            return con.getServiceList(tenant_id);
+        }
+
+        [WebMethod]
+        public int bindService(int service_id, int tenant_id)
+        {
+           dataConn con = new dataConn();
+            return con.bindService(service_id,tenant_id);
+        }
+
+        [WebMethod]
+        public int bindRequirment(int req_id, int tenant_id)
+        {
+            dataConn con = new dataConn();
+            return con.bindRequirement(req_id,tenant_id);        
+        }
+
+        [WebMethod]
+        public ArrayList getServiceSubscriber(int service_id)
+        {
+            dataConn con = new dataConn();
+            return con.getServiceSubscribers(service_id);
+        }
+
+        [WebMethod]
+        public ArrayList getRequirementSubscriber(int req_id)
+        {
+            dataConn con = new dataConn();
+            return con.getRequirementSubscribers(req_id);
+        }
+
+
+
+
+        [WebMethod]
+        public int publishMyRequirements(String description, int tenant_id)
+        {
+            dataConn con = new dataConn();
+
+            if (con.publish_requirement(description, tenant_id) == 200)
+            {
+                String indexPath = @"C:\Users\Shashank\Documents\SD\Project\working\inputFileFS";
+
+                if (!System.IO.Directory.Exists(indexPath))
+                {
+                    System.IO.Directory.CreateDirectory(indexPath);
+                }
+
+                luceneWorkingSet = FSDirectory.Open(indexPath);
+
+                StandardAnalyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+
+                IndexWriter writer = new IndexWriter(luceneWorkingSet, analyzer, true, IndexWriter.MaxFieldLength.LIMITED);
+                Document doc = new Document();
+
+                doc.Add(new Field("req_description", description, Field.Store.YES, Field.Index.ANALYZED));
+                doc.Add(new Field("tenant_id", tenant_id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+
+                writer.AddDocument(doc);
+                writer.Optimize();
+
+                writer.Dispose();
+                luceneWorkingSet.Dispose();
+                return 200;
+            }
+
+            else
+            {
+                return 400;
+            }
+
         }
 
     }
--- SmartCollab/SmartCollab/dataConn.cs
+++ SmartCollab/SmartCollab/dataConn.cs
@@ -2,23 +2,308 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-//using System.Data.Sql;
 using System.Data.SqlClient;
 using System.Data.OleDb;
+using System.Collections;
+
 
 namespace SmartCollab
 {
     public class dataConn
     {
-        public SqlConnection conn = new SqlConnection();
         
-        public void setUpConnection()
+        
+        public int publishService(String service_URL, String service_description, int tenant_id)
         {
+            SqlConnection conn = new SqlConnection();
             conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
             SqlCommand qry = new SqlCommand();
-            qry.CommandText = "Select * from ";
-            qry.CommandType = System.Data.CommandType.Text;
+            String qryText = "insert into service_dir values (((select MAX (service_id) from service_dir)+1), " + "'" + service_URL + "'," + "'" + service_description + "'," + tenant_id.ToString() + ",1);";
+            qry.CommandText = qryText;
+            qry.Connection = conn;
+            
+            try
+            {
+                qry.ExecuteNonQuery();
+                return 200;
+            }
+            catch (Exception e)
+            {
+               return 400;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            
 
         }
+
+        public int unpublishService(int service_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "update service_dir set published=0 where service_id= "+service_id.ToString()+";";
+            query.Connection = conn;
+            try
+            {
+                query.ExecuteNonQuery();
+                return 200;
+            }
+            catch
+            {
+                return 400;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        
+        }
+
+        public bool isPublished(String regEx)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "select published from service_dir where service_name like '%"+regEx+"%';";
+            query.Connection = conn;
+
+            try
+            {
+                return (int.Parse(query.ExecuteScalar().ToString())==1);
+                
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public int unpublish_req(int req_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "delete from requirement where req_id = " + req_id.ToString() + ";";
+            query.Connection = conn;
+            try
+            {
+                query.ExecuteNonQuery();
+                return 200;
+            }
+            catch
+            {
+                return 400;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        
+        }
+
+        public int getReqOwnerId(int req_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "select publisher_tenant_id from requirement where req_id = " + req_id.ToString() + ";";
+            query.Connection = conn;
+            try
+            {
+                return (int)query.ExecuteScalar();
+               
+            }
+            catch
+            {
+                return 400;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        
+        }
+
+        public int publish_requirement(String req_desc, int tenant_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "insert into requirements values(((select MAX (req_id) from requirements)+1), '" + req_desc + "'," + tenant_id.ToString() + ");";
+            query.Connection = conn;
+            try
+            {
+                query.ExecuteNonQuery();
+                return 200;
+            }
+            catch
+            {
+                return 400;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public ArrayList getServiceList(int tenant_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "select service_name from service_dir where tenant_id=" + tenant_id.ToString()+";";
+            query.Connection = conn;
+            ArrayList al = new ArrayList();
+          
+            try
+            {
+                SqlDataReader reader = query.ExecuteReader();
+                while (reader.Read())
+                {
+                    al.Add(reader["service_name"].ToString());
+                   
+                }
+                return al;
+            }
+            catch
+            {
+                al.Add("400");
+                return al;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        public int bindService(int service_id, int tenant_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "insert into service_bind values (" + service_id.ToString()+","+tenant_id.ToString() + ");";
+            query.Connection = conn;
+            try
+            {
+                query.ExecuteNonQuery();
+                return 200;
+            }
+            catch
+            {
+                return 400;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public int bindRequirement(int req_id, int tenant_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "insert into req_bind values (" + req_id.ToString() + "," + tenant_id.ToString() + ");";
+            query.Connection = conn;
+            try
+            {
+                query.ExecuteNonQuery();
+                return 200;
+            }
+            catch
+            {
+                return 400;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public ArrayList getServiceSubscribers(int service_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "select tenant_id from service_bind where service_id=" + service_id.ToString() + ";";
+            query.Connection = conn;
+            ArrayList al = new ArrayList();
+
+            try
+            {
+                SqlDataReader reader = query.ExecuteReader();
+                while (reader.Read())
+                {
+                    al.Add(int.Parse(reader["tenant_id"].ToString()));
+
+                }
+                return al;
+            }
+            catch
+            {
+                al.Add(int.Parse("400"));
+                return al;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        public ArrayList getRequirementSubscribers(int req_id)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=SHASHANK-PC;Initial Catalog=master;Integrated Security=True";
+            conn.Open();
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "select tenant_id from req_bind where req_id=" + req_id.ToString() + ";";
+            query.Connection = conn;
+            ArrayList al = new ArrayList();
+
+            try
+            {
+                SqlDataReader reader = query.ExecuteReader();
+                while (reader.Read())
+                {
+                    al.Add(int.Parse(reader["tenant_id"].ToString()));
+
+                }
+                return al;
+            }
+            catch
+            {
+                al.Add(int.Parse("400"));
+                return al;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+
+
+
+
     }
 }
